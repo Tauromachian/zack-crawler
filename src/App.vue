537c83a6621d0d7318fdac5b1a9811f746984cb2
Jose Garcia <jogarcia@tuta.io>
2022-04-11 15:40:44 -0400

    Load page on enter keypress

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
       <base-input label="URL" v-model="url"></base-input>
       <div class="flex">
         <button
+          @keydown.enter="getPage"
           @click="getPage"
           type="button"
           class="button inline-block px-6 py-2 text-2xs rounded shadow-md hover:shadow-lg active:shadow-lg mt-5"
@@ -45,6 +46,14 @@ export default {
       message: { text: "", status: "", success: true, active: false },
       refreshIframe: 0,
     };
+  },
+  mounted() {
+    const body = document.body;
+    body.addEventListener("keyup", (event) => {
+      if (event.key === "Enter") {
+        this.getPage();
+      }
+    });
   },
   methods: {
     async getPage() {
