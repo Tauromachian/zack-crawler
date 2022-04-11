6703119a47f60a7a5ade39e62a4d7cee46cb021f
Jose Garcia <jogarcia@tuta.io>
2022-04-11 15:20:51 -0400

    Make card component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="block p-6 rounded-lg shadow-lg bg-white">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppCard",
+};
+</script>
+
+<style></style>
