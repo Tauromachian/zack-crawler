5e435d42b7205879080409680613f25ae6a62a81
Jose Garcia <jogarcia@tuta.io>
2022-04-11 17:24:10 -0400

    Make a base button component

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,7 @@
     <app-card class="max-w-sm">
       <base-input label="URL" v-model="url"></base-input>
       <div class="flex">
-        <button
-          @keydown.enter="getPage"
-          @click="getPage"
-          type="button"
-          class="button inline-block px-6 py-2 text-2xs rounded shadow-md hover:shadow-lg active:shadow-lg mt-5"
-        >
-          Button
-        </button>
+        <base-button @click="getPage">Button</base-button>
       </div>
     </app-card>
 
@@ -37,10 +30,11 @@
 
 <script>
 import BaseInput from "./components/BaseInput.vue";
+import BaseButton from "./components/BaseButton.vue";
 import AppCard from "./components/AppCard.vue";
 
 export default {
-  components: { BaseInput, AppCard },
+  components: { BaseInput, AppCard, BaseButton },
   data() {
     return {
       url: "",
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    @keydown.enter="getPage"
+    v-bind="$attrs"
+    @click="getPage"
+    type="button"
+    class="button inline-block px-6 py-2 text-2xs rounded shadow-md hover:shadow-lg active:shadow-lg mt-5"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
