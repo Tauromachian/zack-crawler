509ef09b6c1901ce721f1e0686a140f230dffdbe
Jose Garcia <jogarcia@tuta.io>
2022-04-11 14:46:27 -0400

    Add basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <div id="nav"><router-link to="/">Home</router-link> |</div>
-  <router-view />
+  <div class="flex flex-col justify-center items-center h-screen">
+    <div>
+      <h1>Crawler</h1>
+    </div>
+    <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+      <base-input label="URL" v-model="url"></base-input>
+      <div class="flex">
+        <button
+          type="button"
+          class="button inline-block px-6 py-2 text-2xs rounded shadow-md hover:shadow-lg active:shadow-lg mt-5"
+        >
+          Button
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
