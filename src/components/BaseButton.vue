12b3448643d3cfd37a438d7894cac8d2df0bcc00
Jose Garcia <jogarcia@tuta.io>
2022-04-11 20:55:37 -0400

    Add loading logic

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -6,12 +6,36 @@
     type="button"
     class="button inline-block px-6 py-2 text-2xs rounded shadow-md hover:shadow-lg active:shadow-lg mt-5"
   >
-    <slot></slot>
+    <div v-if="loading" class="flex justify-center items-center">
+      <div
+        class="spinner-border animate-spin inline-block w-6 h-6 border-4 border-top-primary rounded-full"
+        role="status"
+      />
+      <span v-if="loadingText" class="ml-2">{{ loadingText }}</span>
+      <span v-else class="ml-2">Loading...</span>
+    </div>
+    <slot v-else></slot>
   </button>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "BaseButton",
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.spinner-border {
+  border-color: black !important;
+}
+
+.border-top-primary {
+  border-top-color: #96e6b3 !important;
+}
+</style>
